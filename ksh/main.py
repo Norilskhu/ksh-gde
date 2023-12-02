15b90ee6d5e8fa066bcf4c5da6730cc3358d773c
zilahip <peter.zilahi@nitrowise.com>
2023-12-02 14:57:59 +0100

    linearis regresszio + excel v1

--- ksh/main.py
+++ ksh/main.py
@@ -1,5 +1,11 @@
 import csv
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy import stats
+import openpyxl
+from openpyxl.workbook import Workbook
+from sklearn.linear_model import LinearRegression
+
 
 # Ures tomb inicializalasa, amiben a sorok adatait fogjuk kulcs-ertekben tarolni
 data = []
@@ -46,5 +52,42 @@ plt.plot(years, total_values, marker='o', linestyle='None')
 plt.title("Adott év teljes inflációs változása")
 plt.xlabel("Év")
 plt.ylabel("Érték (százalékban)")
+plt.grid(True)
+plt.show()
+
+# Az openpyxl munkafüzet létrehozása
+wb = Workbook()
+ws = wb.active
+
+# Adatok felvitele az Excel fájlba
+# Először a fejléc
+ws.append(header)
+
+# Majd az adatok soronként
+for entry in data:
+    row = [entry[h] for h in header]
+    ws.append(row)
+
+# Excel fájl mentése
+excel_filename = "../xls/inflacio.xlsx"
+wb.save(excel_filename)
+
+# Évek és élelmiszerárak (vagy összes infláció) konvertálása numpy tömbbé
+x = np.array(total_values).reshape(-1, 1)
+y = np.array(food_prices)  # Vagy y = np.array(total_values)
+
+# Lineáris regresszió végrehajtása
+slope, intercept, r_value, p_value, std_err = stats.linregress(x.squeeze(), y)
+
+# Lineáris regressziós egyenes létrehozása
+y_pred = intercept + slope * x.squeeze()
+
+# Ábra megjelenítése
+plt.figure(figsize=(10, 6))
+plt.scatter(x, y, color='blue')  # Adatpontok
+plt.plot(x, y_pred, color='red')  # Regressziós egyenes
+plt.title("Lineáris regresszió az élelmiszerárakra vonatkozó és a teljes inflációs százalék között")
+plt.xlabel("Teljes infláció (százalékban)")
+plt.ylabel("Élelmiszerár (százalékban)")
 plt.grid(True)
 plt.show()
